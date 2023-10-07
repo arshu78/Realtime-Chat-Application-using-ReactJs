891080664fc8c19e9d87267d8ab10ceaf2059e4c
arshad <arshadshirgave@gmail.com>
2023-10-07 13:39:15 +0530

    changes applied

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import ContextFun from "./context/ContextProvider";
+import {Helmet} from "react-helmet";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContextFun>
+      <Helmet>
+                <meta charSet="utf-8" />
+                <title>Fun Chat</title>
+            </Helmet>
+      <Navbar />
+      <Home />
+    </ContextFun>
   );
 }
 
---
+++ src/components/Welcome.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div className="welcome">
+      <h1>Welcome to Fun Chat Messenger</h1>
+      <p>
+        Welcome to Fun Chat App. Lets make fun together. Below is dummy text.
+        Please don't decode it.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+        necessitatibus quos nulla deserunt error qui quia modi suscipit!
+        Deleniti totam alias esse assumenda. Neque qui, quia impedit libero
+        veritatis quas!
+
+        Created By Arshad Shirgave
+      </p>
+    </div>
+  );
+};
+
+export default Welcome;
